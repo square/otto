0fd4714ed447ad277ecea72dd3447b238aa4a163
Jake Wharton <jw@squareup.com>
2012-06-26 10:29:19 -0700

    Code review and build system fix.

--- sample/src/main/java/com/squareup/otto/sample/BusProvider.java
+++ sample/src/main/java/com/squareup/otto/sample/BusProvider.java
@@ -20,7 +20,7 @@ import com.squareup.otto.Bus;
 
 /**
  * Maintains a singleton instance for obtaining the bus. Ideally this would be replaced with a more efficient means
- * such as injection directly into interested classes with Guice.
+ * such as through injection directly into interested classes.
  */
 public final class BusProvider {
   private static final Bus BUS = new Bus();
