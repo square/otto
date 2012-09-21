5369c3d37e58da90f31a253d34088266aa984ff4
Eric Burke <eric@squareup.com>
2012-09-20 21:28:51 -0700

    Updated some comments; mostly focused on replacing passive with active voice

--- library/src/main/java/com/squareup/otto/ThreadEnforcer.java
+++ library/src/main/java/com/squareup/otto/ThreadEnforcer.java
@@ -19,28 +19,28 @@ package com.squareup.otto;
 import android.os.Looper;
 
 /**
- * Enforce the actions for a particular event bus are occurring on a valid thread.
+ * Enforces a thread confinement policy for methods on a particular event bus.
  *
  * @author Jake Wharton
  */
 public interface ThreadEnforcer {
 
   /**
-   * Enforce a valid thread for the given {@code bus}.
+   * Enforce a valid thread for the given {@code bus}. Implementations may throw any runtime exception.
    *
    * @param bus Event bus instance on which an action is being performed.
    */
   void enforce(Bus bus);
 
 
-  /** A {@link ThreadEnforcer} which does no verification. */
+  /** A {@link ThreadEnforcer} that does no verification. */
   ThreadEnforcer ANY = new ThreadEnforcer() {
     @Override public void enforce(Bus bus) {
       // Allow any thread.
     }
   };
 
-  /** A {@link ThreadEnforcer} which ensures you are operating on the main thread. */
+  /** A {@link ThreadEnforcer} that confines {@link Bus} methods to the main thread. */
   ThreadEnforcer MAIN = new ThreadEnforcer() {
     @Override public void enforce(Bus bus) {
       if (Looper.myLooper() != Looper.getMainLooper()) {
