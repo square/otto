9b47754a49c38c16e9e185a032654f4faa8fe9c9
Mark Renouf <mark.renouf@gmail.com>
2012-10-21 09:06:49 -0400

    Default priority level should be 'last'

--- library/src/main/java/com/squareup/otto/Produce.java
+++ library/src/main/java/com/squareup/otto/Produce.java
@@ -32,5 +32,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Produce {
-  int priority() default 0;
+  int priority() default Integer.MAX_VALUE;
 }
