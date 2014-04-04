92c26bde4ed6cc1f60eb0d511e1c2649cebbaf0d
Patrick Forhan <pforhan@squareup.com>
2014-04-04 15:59:20 -0500

    Pass exception message through to generated exceptions
    See related #117 and #116

--- library/src/main/java/com/squareup/otto/DispatchQueue.java
+++ library/src/main/java/com/squareup/otto/DispatchQueue.java
@@ -37,9 +37,9 @@ final class DispatchQueue {
   private static void throwRuntimeException(String msg, InvocationTargetException e) {
     Throwable cause = e.getCause();
     if (cause != null) {
-      throw new RuntimeException(msg, cause);
+      throw new RuntimeException(msg + ": " + cause.getMessage(), cause);
     } else {
-      throw new RuntimeException(msg);
+      throw new RuntimeException(msg + ": " + e.getMessage(), e);
     }
   }
 
